b9c89d5cde80533e708f2b978da426459a691333
안서진 <ahntoday@an-oneul-ui-MacBook-Pro.local>
2020-11-18 14:55:49 +0900

    add: base 작업

---
+++ signal_soriwa/soriwa/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SoriwaConfig(AppConfig):
+    name = 'soriwa'
---
+++ signal_soriwa/soriwa/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.http.response import StreamingHttpResponse
+from soriwa.faceCam import FaceCamera
+from soriwa.motionCam import MotionCamera
+
+# Create your views here.
+
+
+def main(request):
+    return render(request, 'main.html')
+
+
+def emotion(request):
+    return render(request, 'emotion.html')
+
+
+def motion(request):
+    return render(request, 'motion.html')
+
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
+def face(request):
+    return render(request, 'face.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+def face_video(request):
+    return StreamingHttpResponse(gen(FaceCamera()), content_type='multipart/x-mixed-replace; boundary=frame')
+
+
+def motion_video(request):
+    return StreamingHttpResponse(gen(MotionCamera()), content_type='multipart/x-mixed-replace; boundary=frame')
+
+
+def emotion_analysis(request):
+    return render(request, 'emotion_analysis.html')
+
+
+def analysis_result(reqeust):
+    return render(request, 'analysis_result.html')
